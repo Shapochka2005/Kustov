a53817ecd386cc07e5b97188de2d757f25718b76
Karls0n <85025692+Shapochka2005@users.noreply.github.com>
2023-02-16 22:24:31 +0300

    Add files via upload

---
+++ Кустов9/Кустов9/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Кустов9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Main();
+        }
+    }
+}
