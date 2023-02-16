0fcde347bc67eb577750f2f6b26d35cab74ff761
Karls0n <85025692+Shapochka2005@users.noreply.github.com>
2023-02-16 22:38:29 +0300

    Add files via upload

---
+++ Кустов8/Кустов8/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Кустов8
+{
+    internal class User
+    {
+        public string username;
+        public int sInMin;
+        public double sInSec;
+    }
+}
---
+++ Кустов8/Кустов8/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Кустов8
+{
+    public class Program
+    {
+        public static bool start = false;
+
+        static void Main(string[] argrs)
+        {
+            User user = new User();
+            bool flag = false;
+            Thread ts = new Thread(_ =>
+            {
+                while (true)
+                {
+                    Stopwatch t = new Stopwatch();
+
+                    if (start == true)
+                    {
+                        t.Start();
+                        while (true)
+                        {
+                            if (t.ElapsedMilliseconds / 1000 == 60)
+                            {
+                                t.Stop();
+                                Console.WriteLine($"\n Минута прошла!");
+                                start = false;
+                                break;
+                            }
+                        }
+                    }
+
+                }
+
+            });
+            ts.Start();
+
+
+            while (!flag)
+            {
+                Console.WriteLine("Ввведите имя для таблицы рекордов:");
+
+                string name = Console.ReadLine();
+                user.username = name;
+                Console.Clear();
+                start = true;
+                user.sInMin = PrintText.InpText();
+                user.sInSec = user.sInMin / 60.0;
+
+                string text = $"\n {user.username} {user.sInSec} {user.sInMin}";
+                File.AppendAllText("C:\\Users\\Kliker\\source\\repos\\ScorePech\\records.txt", text);
+                string records = File.ReadAllText("C:\\Users\\Kliker\\source\\repos\\ScorePech\\records.txt");
+                Console.WriteLine("Таблица рекордов");
+                Console.WriteLine(records);
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
